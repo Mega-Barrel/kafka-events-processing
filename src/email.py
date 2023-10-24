2486fab7b8f8c7c140d33911f7184e572fa5d7c2
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-10-24 23:15:13 +0530

    email service to read data and generate total emails send

---
+++ src/email.py
@@ -0,0 +1,25 @@
+import json
+
+from kafka import (
+    KafkaConsumer
+)
+
+ORDER_CONFIRMED_KAFKA_TOPIC = "order_confirmed"
+
+consumer = KafkaConsumer(
+    ORDER_CONFIRMED_KAFKA_TOPIC,
+    bootstrap_servers = 'localhost:29092'
+)
+
+email_sent_so_far = set()
+
+print("Listening new email data ...")
+
+while True:
+    for message in consumer:
+        consumed_message = json.loads(message.value.decode())
+        customer_email = consumed_message['email']
+
+        print(f'Sending email to {customer_email}.')
+        email_sent_so_far.add(customer_email)
+        print(f'So far email sent to {len(email_sent_so_far)} unique customers')
