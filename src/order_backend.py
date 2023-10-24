be309ab4cfb637fa186548fc65ffdddb275b8eae
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-10-24 23:14:11 +0530

    updated order limit to 20k records

--- src/order_backend.py
+++ src/order_backend.py
@@ -4,19 +4,16 @@ import time
 from kafka import KafkaProducer
 
 ORDER_KAFKA_TOPIC = "order_details"
-ORDER_LIMIT = 15
+ORDER_LIMIT = 20000
 
 # produce events
-producer = KafkaProducer(boostrap_servers="localhost:29092")
-
-print("Generating orders after 10 seconds.")
-print("Generate one unique order every 10 seconds.")
+producer = KafkaProducer(bootstrap_servers='localhost:29092')
 
 for i in range(1, ORDER_LIMIT):
     data = {
         "order_id": i,
         "user_id": f"user_{i}",
-        "total_cost": i * 5,
+        "total_cost": i * 2,
         "items": "burger, sandwich"
     }
 
@@ -24,6 +21,5 @@ for i in range(1, ORDER_LIMIT):
         ORDER_KAFKA_TOPIC,
         json.dumps(data).encode("utf-8")
     )
-    
+
     print(f"Done sending...{i}")
-    time.sleep(10)
